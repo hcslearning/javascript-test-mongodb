0446528496cd90095f9fd0e6ec7354171d2901e6
HCS Learning <learning@hardcybersoft.cl>
2021-03-13 21:51:39 -0300

    test con MongoDB in Memory

--- test/db.test.js
+++ test/db.test.js
@@ -1,4 +1,30 @@
-test('prueba', () => {
-    expect(1).toBe(1)
-})
+const {MongoClient} = require('mongodb');
+
+describe('insert', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    db = await connection.db();
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
+  });
+
+  it('should insert a doc into collection', async () => {
+    const users = db.collection('users');
+
+    const mockUser = {_id: 'some-user-id', name: 'John'};
+    await users.insertOne(mockUser);
+
+    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    expect(insertedUser).toEqual(mockUser);
+  });
+});
 
